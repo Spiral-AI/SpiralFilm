a1eaa3ae55725a1a24e8ee8559354db10a5a1e79
Yuichi Sasaki <y_sasaki@go-spiral.ai>
2023-07-10 22:51:43 +0900

    ある程度作った。

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SpiralFilm",
+    version="0.1.0",
+    url="https://github.com/Spiral-AI/spiralfilm",
+    author="Yuichi Sasaki",
+    author_email="y_sasaki@go-spiral.ai",
+    description="A thin wrapper for the OpenAI GPT family of APIs",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
---
+++ spiral_film/__init__.py
@@ -0,0 +1,9 @@
+# spiral_film/__init__.py
+
+# Import main classes and functions from the relevant modules
+from .core import MainClass1, MainClass2, main_function
+from .utils import utility_function1, utility_function2
+from .exceptions import CustomException1, CustomException2
+
+# Specify the version of the package
+__version__ = "0.1.0"
