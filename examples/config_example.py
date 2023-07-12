aeb9d07578d6e446c4f4d36936eae383eb8b4aad
Yuichi Sasaki <y_sasaki@go-spiral.ai>
2023-07-12 13:24:53 +0900

    Update SpiralFilm wrapper with automatic retry and placeholder functionality

---
+++ examples/config_example.py
@@ -0,0 +1,18 @@
+from spiral_film import FilmCore, FilmConfig
+
+# Ensure you have set the OPENAI_API_KEY environment variable
+# import os
+# os.environ["OPENAI_API_KEY"] = "your key here"
+
+config = FilmConfig(model="gpt-4", temperature=0.5, max_tokens=100)
+_template = """
+Talk as you want.
+You're {{user_name}}.
+"""
+
+f = FilmCore(
+    prompt=_template,
+    config=config,
+).run(placeholders={"user_name": "Tom"})
+
+print(f)
--- spiral_film/__init__.py
+++ spiral_film/__init__.py
@@ -1,9 +1,8 @@
 # spiral_film/__init__.py
 
 # Import main classes and functions from the relevant modules
-from .core import MainClass1, MainClass2, main_function
-from .utils import utility_function1, utility_function2
-from .exceptions import CustomException1, CustomException2
+from .core import FilmCore
+from .config import FilmConfig
 
 # Specify the version of the package
 __version__ = "0.1.0"
