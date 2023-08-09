496da315291f9d5b2719dc306031034cd4176859
Yuichi Sasaki <y_sasaki@go-spiral.ai>
2023-08-09 22:59:59 +0900

    公開するためにいくつか追加

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="SpiralFilm",
     version="0.1.0",
@@ -9,11 +12,17 @@ setup(
     description="A thin wrapper for the OpenAI GPT family of APIs",
     packages=find_packages(),
     install_requires=["openai", "tiktoken"],
+    python_requires=">=3.8, <4",
+    keywords="openai, gpt, api, wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
