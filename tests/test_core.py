6a24733b532e24461dce49b449289db186ae7ce4
Yuichi Sasaki <y_sasaki@go-spiral.ai>
2023-12-15 15:17:40 +0900

    updated

--- tests/test_core.py
+++ tests/test_core.py
@@ -26,41 +26,86 @@ def mock_env_vars():
         yield
 
 
-# FilmCore.run メソッドのテスト
-def test_run(film_core_instance, mock_openai_api, mock_env_vars):
-    # 期待される応答をモックする
+# 最もシンプルな生成のテスト
+def test_run(mock_openai_api, mock_env_vars):
+    # 期待される応答をモックする: https://platform.openai.com/docs/api-reference/making-requests
     expected_response = {
+        "id": "chatcmpl-abc123",
+        "object": "chat.completion",
+        "created": 1677858242,
+        "model": "gpt-3.5-turbo-1106",
+        "usage": {"prompt_tokens": 13, "completion_tokens": 7, "total_tokens": 20},
         "choices": [
             {
-                "message": {"content": "Mocked response"},
-                "finish_reason": "length",
+                "message": {"role": "assistant", "content": "Hi, I'm feeling good!"},
+                "finish_reason": "stop",
                 "index": 0,
             }
         ],
-        "usage": {"completion_tokens": 17, "prompt_tokens": 57, "total_tokens": 74},
     }
     mock_openai_api.ChatCompletion.create.return_value = expected_response
 
+    # FilmCore クラスのインスタンスを生成。なるべく多く引数を設定してみる。
+    film_core_instance = FilmCore(
+        prompt="""Hi {{name}}. Talk as you want.""",
+        config=FilmConfig(model="gpt-3.5-turbo-1106", temperature=0.0, max_tokens=100),
+    )
+
     # placeholdersを用いてrunメソッドを呼び出す
-    result = film_core_instance.run({"variable1": "summer", "variable2": "hot"})
+    result = film_core_instance.run({"name": "Tom"})
 
     # 結果が期待通りであることをアサートする
-    assert result == "Mocked response"
-    assert film_core_instance.finished_reason == "length"
+    assert result == expected_response["choices"][0]["message"]["content"]
+    assert (
+        film_core_instance.finished_reason
+        == expected_response["choices"][0]["finish_reason"]
+    )
     assert film_core_instance.token_usages == expected_response["usage"]
 
 
 # _placeholder メソッドのテスト
-@pytest.mark.skip(reason="このテストは現在無効化されています")
-def test_placeholder(film_core_instance):
+def test_placeholder(mock_openai_api):
     # プロンプトに含まれるプレースホルダーを置き換え
-    original_prompt = film_core_instance.prompt
-    placeholders = {"name": "Alice", "day": "Monday"}
-    updated_prompt = film_core_instance._placeholder(original_prompt, placeholders)
+    prompt = "{{variable1}} is {{variable2}}. And {{variable3}} is {{variable4}}."
 
-    # 各プレースホルダーが適切に置き換えられたかをチェック
-    for key, value in placeholders.items():
-        assert value in updated_prompt
+    fc = FilmCore(prompt)
+
+    # 正常系の確認
+    placeholders = {
+        "variable1": "summer",
+        "variable2": "hot",
+        "variable3": "winter",
+        "variable4": "cold",
+    }
+    print(fc.placeholders())
+    assert fc.placeholders() == ["variable1", "variable2", "variable3", "variable4"]
+    assert fc._placeholder(prompt, placeholders) == "summer is hot. And winter is cold."
+
+    # 異常系の確認 - variableが足りないケース
+    placeholders = {"variable1": "summer", "variable2": "hot"}
+    with pytest.raises(ValueError):
+        fc._placeholder(prompt, placeholders)
+
+    # 異常系の確認 - variableが間違っているケース
+    placeholders = {
+        "variable1": "summer",
+        "variable2": "hot",
+        "variable3": "winter",
+        "hogehoge": "cold",
+    }
+    with pytest.raises(ValueError):
+        fc._placeholder(prompt, placeholders)
+
+    # 異常系の確認 - variableが余分なケース
+    placeholders = {
+        "variable1": "summer",
+        "variable2": "hot",
+        "variable3": "winter",
+        "variable4": "cold",
+        "hogehoge": "fugafuga",
+    }
+    with pytest.raises(ValueError):
+        fc._placeholder(prompt, placeholders)
 
 
 # _messages メソッドのテスト
